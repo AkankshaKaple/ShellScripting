15854795c9a31b98866fc4b1d82c2fd4e586648d
AkankshaKaple <akankshakaple@gmail.com>
2019-11-19 15:08:14 +0530

    Add: Function to make bet for 20 days

--- GamblingSimulator/gambling_simulator.sh
+++ GamblingSimulator/gambling_simulator.sh
@@ -8,9 +8,10 @@ amountLeft=$STAKE
 Won=1
 MAX_AMOUNT=$(( ($STAKE*50)/100 ))
 MIN_AMOUNT=-$MAX_AMOUNT
+NUMBER_OF_DAYS=20
 
 function makeBet() {
-	local betStatus=$((RANDOM%2))
+	betStatus=$((RANDOM%2))
 	if [ $Won -eq $betStatus ]
 	then
 		echo $AMOUNT_BET
@@ -21,8 +22,9 @@ function makeBet() {
 
 
 function updateAmount() {
-	local betAmount="$( makeBet )"
+	betAmount="$( makeBet )"
 	amountLeft=$((  $amountLeft + $betAmount ))
+	echo $amountLeft
 }
 
 
@@ -35,7 +37,25 @@ function dailyBetting() {
 	fi
 }
 
-dailyBetting
 
+function bettingTwentyDays() {
+	x=$NUMBER_OF_DAYS
+	while [ $x -gt 0 ]
+   do
+      dailyBetting
+		x=$(($x-1))
+   done
+}
 
+function monthlyBetting() {
+	for (( day=1; day <= $NUMBER_OF_DAYS; day++ ))
+	do
+   	dailyBetting
+		BetResult=$(( $BetResult + $amountLeft ))
+		amountLeft=0
+	done
+	echo $totalBetResult
+}
+
+bettingTwentyDays
 
