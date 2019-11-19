7f4b4ed2bc2b26f4eb1f2e6698a35e2c6f94bab0
AkankshaKaple <akankshakaple@gmail.com>
2019-11-19 12:21:18 +0530

    Add: Added a funtion to check if user has reached max and min amount

--- GamblingSimulator/gambling_simulator.sh
+++ GamblingSimulator/gambling_simulator.sh
@@ -6,6 +6,8 @@ LOSS=0
 AMOUNT_BET=1
 amountLeft=$STAKE
 Won=1
+MAX_AMOUNT=$(( ($STAKE*50)/100 ))
+MIN_AMOUNT=-$MAX_AMOUNT
 
 function makeBet() {
 	local betStatus=$((RANDOM%2))
@@ -18,9 +20,22 @@ function makeBet() {
 }
 
 
-function dailyBet() {
+function updateAmount() {
 	local betAmount="$( makeBet )"
 	amountLeft=$((  $amountLeft + $betAmount ))
 }
 
-dailyBet
+
+function dailyBetting() {
+	if [ $MAX_AMOUNT -eq $amountLeft -o $MIN_AMOUNT -eq $amountLeft ]
+	then
+		echo "Quit the game"
+	else
+		updateAmount
+	fi
+}
+
+dailyBetting
+
+
+
