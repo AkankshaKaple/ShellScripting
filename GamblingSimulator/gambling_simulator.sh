62bf33f30db04d8bf0fa5a3523c45f005d13b28b
AkankshaKaple <akankshakaple@gmail.com>
2019-11-19 11:36:48 +0530

    Add: Added a function to do the betting and return amount left.

--- GamblingSimulator/gambling_simulator.sh
+++ GamblingSimulator/gambling_simulator.sh
@@ -2,3 +2,25 @@ echo "Welcome to my gambling simulator"
 
 STAKE=100
 BET=1
+LOSS=0
+AMOUNT_BET=1
+amountLeft=$STAKE
+Won=1
+
+function makeBet() {
+	local betStatus=$((RANDOM%2))
+	if [ $Won -eq $betStatus ]
+	then
+		echo $AMOUNT_BET
+	else
+		echo -$AMOUNT_BET
+	fi
+}
+
+
+function dailyBet() {
+	local betAmount="$( makeBet )"
+	amountLeft=$((  $amountLeft + $betAmount ))
+}
+
+dailyBet
